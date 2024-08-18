bb8fde2a20f8468824607adaf7573e5702af04b1
wendy <mizukilee@gmail.com>
2024-08-18 17:14:21 -0400

    conditional

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import './components/Chicken'
 import Chicken from './components/Chicken';
+import DoubleDice from './components/DoubleDice';
+
 
 function App() {
   return (
     <div className="App">
-      <Chicken/>
+      <DoubleDice/>
+      <DoubleDice/>
+      <DoubleDice/>
     </div>
   );
 }
