903814d57744ff5464db3a87e625a9e963b934bd
wendy <mizukilee@gmail.com>
2024-08-18 16:43:13 -0400

    props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import './App.css';
 import './components/Chicken'
+import './components/Greeter'
 import Chicken from './components/Chicken';
+<<<<<<< HEAD
 import DoubleDice from './components/DoubleDice';
 
+=======
+import Greeter from './components/Greeter';
+>>>>>>> 11e296b (props)
 
 function App() {
   return (
     <div className="App">
+<<<<<<< HEAD
       <DoubleDice/>
       <DoubleDice/>
       <DoubleDice/>
+=======
+      <Chicken/>
+      <Greeter person="Wendy"/>
+      <Greeter person="Cindy"/>
+      <Greeter person="Anthony"/>
+
+
+>>>>>>> 11e296b (props)
     </div>
   );
 }
