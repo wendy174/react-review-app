72fe9fb13bbc6ec7ca9763f3c68b6aaf94b92cde
wendy <mizukilee@gmail.com>
2024-08-18 21:03:41 -0400

    dynamic styling

--- src/App.js
+++ src/App.js
@@ -4,11 +4,18 @@ import './components/Greeter'
 import Chicken from './components/Chicken';
 import Greeter from './components/Greeter';
 import DoubleDice from './components/DoubleDice';
+import Header from './components/Header';
+
 
 
 function App() {
   return (
     <div className="App">
+      <Header text="Welcome!"/>
+      <Header color="blue" text="Win or Die!"/>
+
+      <DoubleDice/>
+      <DoubleDice/>
       <DoubleDice/>
     </div>
   );
--- src/components/DoubleDice.js
+++ src/components/DoubleDice.js
@@ -1,14 +1,17 @@
 export default function DoubleDice(){
     const num1 = Math.floor(Math.random() * 3) + 1; 
     const num2 = Math.floor(Math.random() * 3) + 1; 
-
+    const isWinner = num1 === num2; 
+    const styles = { color: isWinner ? "green" : "red" }
+    // green if win 
+    // red if lose
     return ( 
-        <>
+        <div className="DoubleDice" style={styles}>
             <h2>DoubleDice</h2>
             {num1 === num2 && <h3> You win!  </h3>}
             <p>Num1: {num1}</p>
             <p>Num2: {num2}</p>
-        </>
+        </div>
     )
 
 }
