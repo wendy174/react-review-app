0f29ce48dd3379bd7e3aa3c071414c565773fb78
wendy <mizukilee@gmail.com>
2024-09-05 22:33:09 -0400

    property

--- src/App.tsx
+++ src/App.tsx
@@ -30,14 +30,21 @@ function App(): JSX.Element {
     lastName: 'Doe'
   }
 
-  // const properties = [
-  //   { id: 129031, name: "Desert Yurt", rating: 4.9, price: 150 },
-  //   { id: 129331, name: "Lone Mountain Cabin", rating: 4.8, price: 250 },
-  //   { id: 129032, name: "Cactus Retreat", rating: 4.75, price: 300 },
-  //   { id: 129033, name: "Redwood Treehouse Escape", rating: 4.9, price: 120 },
-  //   { id: 129034, name: "Oceanview Condo", rating: 4.7, price: 140 },
-  //   { id: 129035, name: "Gold Miner Campground", rating: 4.69, price: 96 },
-  // ];
+  interface PropertyItem { 
+    id: number;
+    name: string;
+    rating: number;
+    price: number;
+  }
+
+  const properties: PropertyItem[] = [
+    { id: 129031, name: "Desert Yurt", rating: 4.9, price: 150 },
+    { id: 129331, name: "Lone Mountain Cabin", rating: 4.8, price: 250 },
+    { id: 129032, name: "Cactus Retreat", rating: 4.75, price: 300 },
+    { id: 129033, name: "Redwood Treehouse Escape", rating: 4.9, price: 120 },
+    { id: 129034, name: "Oceanview Condo", rating: 4.7, price: 140 },
+    { id: 129035, name: "Gold Miner Campground", rating: 4.69, price: 96 },
+  ];
 
   return (
     <div className="App">
@@ -52,6 +59,7 @@ function App(): JSX.Element {
       <DoubleDice/>
       <DoubleDice/>
       <DoubleDice/> */}
+      <PropertyList properties={properties}/>
       <PropsExample name='alice' age={30}/>
       <InfoFlow onSelectedItem={handleSelectedItem}/>
       <Header text="hello world"/>
